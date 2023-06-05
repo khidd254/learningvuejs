fb2885c73203e5addc7fcfd3d3f30a7f8b09b30b
khidd <kennedynyambura5981@gmail.com>
2023-06-05 09:21:17 +0300

    init

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-console.log('hello, vue')
+
+const app = Vue.createApp({
+   data() {
+    return{
+      url:'www.khidd.intagram.com',
+        showBooks: true,
+       books: [
+        {title:'Life of Michelle Obama', author: 'nyambura', yearofpublishing:'2010', img: 'assets/1.jpg', isFav: true},
+        {title:'Growth of Kenya', author:'Kennedy T Thuo', yearofpublishing:'2001', img: 'assets/2.jpg', isFav: false},
+        {title:'Rise of Maumau', author:'Vielina M Mbote', yearofpublishing:'2004',  img: 'assets/3.jpg', isFav: true},
+       ],
+         x: 0,
+        y: 0
+    }
+   },
+   methods: {
+    changeTitle(title) {
+      // this.title= 'words of wisdom'
+      this.title= title
+    },
+    toggleshowbooks(){
+        this.showBooks= !this.showBooks
+     },
+     toggleFav(book) {
+      book.isFav = !book.isFav
+    },
+     handleEvent(e, data){
+      console.log(e, e.type)
+      if(data){
+        console.log(data)
+      }
+     },
+     handleMouseMove(e) {
+      this.x = e.offsetX
+      this.y = e.offsetY
+     }
+    
+   },
+   computed: {
+     filteredbooks(){
+       return this.books.filter((book)=> book.isFav)
+     }
+   }
+  
+})
+app.mount('#app')
